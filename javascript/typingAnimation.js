3e72b5f2326a5e1d02f137417a7e9d0d6f5bfc16
김동인 <amiro@amiro.local>
2025-01-26 02:33:20 +0900

    kor resume

---
+++ javascript/typingAnimation.js
@@ -0,0 +1,10 @@
+new Typed('.greeting', {
+  strings: [
+      'Nice to meet you,\n\nI\'m Dongin Kim',
+      '반갑습니다,\n\n저는 김동인입니다.'
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000,
+  loop: false
+});
