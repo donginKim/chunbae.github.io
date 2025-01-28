79b91ed3b2e8608b1c3a4f6c5706e5af72525b09
김동인 <amiro@amiro.local>
2025-01-28 14:53:56 +0900

    package.json homepage 변경

--- src/constants/headerContent.js
+++ src/constants/headerContent.js
@@ -1,16 +1,16 @@
 export const content = {
     ko: {
       title: "김 동 인",
-      about: "소개.",
-      experience: "경력.",
-      skills: "기술.",
-      contact: "연락처."
+      about: "소개",
+      experience: "경력",
+      skills: "기술",
+      contact: "연락처"
     },
     en: {
       title: "DONGIN KIM",
-      about: "About.",
-      experience: "Experience.",
-      skills: "Skills.",
-      contact: "Contact."
+      about: "About",
+      experience: "Experience",
+      skills: "Skills",
+      contact: "Contact"
     }
   };
